6ea4b48bdf64fb09557bb2742c38c4c7d813f2f2
LUIZ CARLOS <50230428+luizcarlosgt@users.noreply.github.com>
2022-04-06 13:04:51 -0500

    Alteração css

--- js/main.js
+++ js/main.js
@@ -3,11 +3,26 @@ var app = new Vue({
     el: '#app',
     data: {
       message: null,
-      letterscount : "0",
+      letterscount : 0,
+      wordscount: 0
     },
     watch:{
         message: function(){
-            return this.letterscount = this.message.length
+            texto = formatarTexto(this.message);
+            this.letterscount = this.message.length
+            this.wordscount = texto.split(" ").length;
         }
     }
   })
+
+  function formatarTexto(texto){
+    var primerBlanco = /^ /
+    var ultimoBlanco = / $/
+    var variosBlancos = /[ ]+/g
+
+    texto = texto.replace (variosBlancos," ");
+    texto = texto.replace (primerBlanco,"");
+    texto = texto.replace (ultimoBlanco,"");
+
+    return texto;
+  }
